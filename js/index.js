95599216986030d8fec85afa9a206e45547a2c72
miyeon8mm <163243942+miyeon8mm@users.noreply.github.com>
2026-06-14 22:36:58 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,11 +57,11 @@ $(function () {
     if($(window).width() > 768){
       const scrollTop = $(this).scrollTop();
 
-      if(scrollTop > 180) $("#section2").addClass("up");
-      if(scrollTop > 1200) $("#work").addClass("up");
-      if(scrollTop > 1200) $("#section4-wrap").addClass("up");
-      if(scrollTop > 3120) $("#section5").addClass("up");
-      if(scrollTop > 3320) $("#section6").addClass("up");
+      if(scrollTop > 300) $("#section2").addClass("up");
+      if(scrollTop > 1174.5) $("#work").addClass("up");
+      if(scrollTop > 1587) $("#section4-wrap").addClass("up");
+      if(scrollTop > 1958) $("#section5").addClass("up");
+      // if(scrollTop > 3320) $("#section6").addClass("up");
     }
     
   });
