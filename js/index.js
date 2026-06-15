00f6e437e5f699bdc3a2ce37725f6c8748076c20
miyeon8mm <163243942+miyeon8mm@users.noreply.github.com>
2026-06-14 23:03:37 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,13 +52,11 @@ $(function () {
     $("html").animate({ scrollTop: 3550 }, 2500);
   });
 
-
   $(window).on("scroll", function(){
-    if($(window).width() > 768){
-      const scrollTop = $(this).scrollTop();
+  const scrollTop = $(this).scrollTop();
+  const winW = $(window).width();
 
-
-       if(winW > 768){
+  if(winW > 768){
     if(scrollTop > 300) $("#section2").addClass("up");
     if(scrollTop > 1174) $("#work").addClass("up");
     if(scrollTop > 1587) $("#section4-wrap").addClass("up");
@@ -69,14 +67,32 @@ $(function () {
     if(scrollTop > 1100) $("#section4-wrap").addClass("up");
     if(scrollTop > 1500) $("#section5").addClass("up");
   }
-      // if(scrollTop > 300) $("#section2").addClass("up");
-      // if(scrollTop > 1174) $("#work").addClass("up");
-      // if(scrollTop > 1587) $("#section4-wrap").addClass("up");
-      // if(scrollTop > 1958) $("#section5").addClass("up");
-      // if(scrollTop > 3320) $("#section6").addClass("up");
-    }
+});
+
+  // $(window).on("scroll", function(){
+  //   if($(window).width() > 768){
+  //     const scrollTop = $(this).scrollTop();
+
+
+  //      if(winW > 768){
+  //   if(scrollTop > 300) $("#section2").addClass("up");
+  //   if(scrollTop > 1174) $("#work").addClass("up");
+  //   if(scrollTop > 1587) $("#section4-wrap").addClass("up");
+  //   if(scrollTop > 1958) $("#section5").addClass("up");
+  // } else {
+  //   if(scrollTop > 200) $("#section2").addClass("up");
+  //   if(scrollTop > 700) $("#work").addClass("up");
+  //   if(scrollTop > 1100) $("#section4-wrap").addClass("up");
+  //   if(scrollTop > 1500) $("#section5").addClass("up");
+  // }
+  //     if(scrollTop > 300) $("#section2").addClass("up");
+  //      if(scrollTop > 1174) $("#work").addClass("up");
+  //      if(scrollTop > 1587) $("#section4-wrap").addClass("up");
+  //      if(scrollTop > 1958) $("#section5").addClass("up");
+  //      if(scrollTop > 3320) $("#section6").addClass("up");
+  //   }
     
-  });
+  // });
 
   $(".list:nth-child(1) > span, .work_h:nth-child(1) img").click(function(){
     var sec3_top = $(window).scrollTop();
